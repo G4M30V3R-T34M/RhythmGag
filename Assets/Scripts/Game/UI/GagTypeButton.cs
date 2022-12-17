324a19c980deb41d2edef6e6ff48c63644548d70
Neengash <pepmanas93@gmail.com>
2022-12-17 14:40:16 +0100

    Add keyboard association to buttons for player to tell gags

--- Assets/Scripts/Game/UI/GagTypeButton.cs
+++ Assets/Scripts/Game/UI/GagTypeButton.cs
@@ -9,11 +9,25 @@ public class GagTypeButton : BaseMenuOption
     [SerializeField] IntGameEvent gameEvent;
     [SerializeField] GagPreference_SO gagPreference;
 
+    [SerializeField] Button button;
+
     private void Start() {
         gagIconImage.sprite = gagPreference.sprite;
         gagIconImage.color = gagPreference.color;
         arrowIconImage.sprite = gagPreference.arrowSprite;
         arrowIconImage.color = gagPreference.color;
+    }
+
+    public void InvokeOnClick() {
+        StartCoroutine(FadeToColor());
+        button.onClick.Invoke();
+    }
+
+    private IEnumerator FadeToColor() {
+        Graphic graphic = GetComponent<Graphic>();
+        graphic.CrossFadeColor(button.colors.pressedColor, button.colors.fadeDuration, true, true);
+        yield return new WaitForSeconds(button.colors.fadeDuration);
+        graphic.CrossFadeColor(button.colors.normalColor, button.colors.fadeDuration, true, true);
     }
 
     public override void MenuOptionAction() {
